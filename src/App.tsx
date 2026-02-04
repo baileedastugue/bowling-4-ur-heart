900ce46f5354d45d60650b26e66b3553ae4bd8f7
Bailee Dastugue <bailee.dast@gmail.com>
2026-02-03 20:30:28 -0600

    file clean up after removing vite defaults and adding additional hemisphere lighting

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Canvas } from '@react-three/fiber'
+import BowlingBall from './components/BowlingBall'
+import './App.css'
+
+function App() {
+  return (
+    <div id="canvas-container">
+      <Canvas id="canvas" camera={{ position: [0, 0, 5], fov: 100, near: 0.1, far: 50 }}>
+        <hemisphereLight groundColor="pink" color="lightblue" intensity={.1} />
+        <directionalLight position={[5, 10, 5]} color="white" />
+        <BowlingBall />
+      </Canvas>
+    </div>
+  )
+}
+
+export default App
