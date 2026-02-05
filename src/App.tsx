979cfdc28e0a885d8b51b70c0b4d083b5ec85acd
Bailee Dastugue <bailee.dast@gmail.com>
2026-02-04 19:54:05 -0600

    added plane and elevated camera to give appearance of bowling alley floor

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Canvas } from '@react-three/fiber'
 import BowlingBall from './components/BowlingBall'
+import Plane from './components/Plane'
 import './App.css'
 
 function App() {
   return (
     <div id="canvas-container">
-      <Canvas id="canvas" camera={{ position: [0, 0, 5], fov: 100, near: 0.1, far: 50 }}>
+      <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 100, near: 0.1, far: 50 }}>
         <color attach="background" args={["purple"]} />
         <hemisphereLight groundColor="pink" color="lightblue" intensity={.1} />
         <directionalLight position={[5, 10, 5]} color="white" />
+        <Plane />
         <BowlingBall />
       </Canvas>
     </div>
---
+++ src/components/Plane/index.tsx
@@ -0,0 +1,10 @@
+const Plane = () => {
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]} receiveShadow>
+      <planeGeometry args={[20, 20]} />
+      <meshStandardMaterial color="#8B4513" />
+    </mesh>
+  )
+}
+
+export default Plane
